458d52448db3b33d6a8356e778c6e4b7c7285de7
Erasmo Freitas <erasmo_sf@hotmail.com>
2021-06-25 01:19:10 -0300

    init implement test

---
+++ ApiTeste1.Test/ApiTeste1.Test/GetTaxaJurosTest.cs
@@ -0,0 +1,15 @@
+﻿using ApiTeste1.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTeste1.Test
+{
+    public class GetTaxaJurosTest : ITaxaJurosApplication
+    {
+        public decimal GetTaxaJuros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
